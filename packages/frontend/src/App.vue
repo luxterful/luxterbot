e79e54687add61c788bc62a95ebd57d348d6513e
Lukas Bauer <lukas.bauer@vogel.de>
2021-12-17 14:31:14 +0100

    adds boilerplate vue frontend

---
+++ packages/frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>Placeholder</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="scss"></style>
---
+++ vetur.config.js
@@ -0,0 +1,5 @@
+// This is a config file for vscode Veture Plugin
+// https://vuejs.github.io/vetur/reference
+module.exports = {
+  projects: ["./packages/frontend"],
+};
