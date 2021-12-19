10222d2dab7c246ee2d15a3b94919e060047df65
Lukas Bauer <lukas.bauer@vogel.de>
2021-12-19 15:19:52 +0100

    use client code in frontend

--- packages/frontend/src/views/Configuration.vue
+++ packages/frontend/src/views/Configuration.vue
@@ -1,48 +1,63 @@
 <template>
   <router-link to="/"> <p class="font-bold">&#60; back to chat</p></router-link>
-  <div class="mt-4">
-    <div class="w-full mb-6">
-      <label
-        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        for="grid-first-name">
-        Webhook URL
-      </label>
-      <input
-        class="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
-        id="grid-first-name"
-        type="text"
-        placeholder="http:// ..." />
+  <template v-if="configuration">
+    <div class="mt-4">
+      <div class="w-full mb-6">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-first-name">
+          Webhook URL
+        </label>
+        <input
+          class="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
+          v-model="configuration.webhookUrl"
+          type="text"
+          placeholder="http:// ..." />
+      </div>
     </div>
-  </div>
-  <div class="mt-4">
-    <div class="w-full mb-6">
-      <label
-        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        for="grid-first-name">
-        Webhook Secret
-      </label>
-      <input
-        class="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
-        id="grid-first-name"
-        type="text"
-        placeholder="Secret ..." />
+    <div class="mt-4">
+      <div class="w-full mb-6">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="grid-first-name">
+          Webhook Secret
+        </label>
+        <input
+          class="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
+          v-model="configuration.webhookSecret"
+          type="text"
+          placeholder="Secret ..." />
+      </div>
     </div>
-  </div>
-  <lux-button @click="saveConfiguration"> Save </lux-button>
+    <lux-button @click="saveConfiguration"> Save </lux-button>
+  </template>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import LuxButton from "@/components/LuxButton.vue";
+import { apiClient, Configuration } from "service-1/dist/client";
 
 export default defineComponent({
   name: "App",
   components: { LuxButton },
   setup() {
+    const configuration = ref<Configuration | null>(null);
+    const client = apiClient("http://localhost:8081");
+
     const saveConfiguration = () => {
-      console.log("save");
+      if (configuration.value) {
+        client.updateConfiguration(configuration.value);
+      }
     };
-    return { saveConfiguration };
+
+    const fetchConfiguration = async () => {
+      configuration.value = await client.getConfiguration();
+    };
+
+    fetchConfiguration();
+
+    return { saveConfiguration, configuration };
   },
 });
 </script>
