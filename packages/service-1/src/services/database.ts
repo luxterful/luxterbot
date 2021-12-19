0a33bb41ffc66632fd4412e7a299b7f367ef8c23
Lukas Bauer <lukas.bauer@vogel.de>
2021-12-19 19:19:11 +0100

    adds database timeout to 5 seconds

--- packages/service-1/src/services/database.ts
+++ packages/service-1/src/services/database.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
 const mongoUri = "mongodb://root:example@localhost:27017/";
-const client = new MongoClient(mongoUri);
+const client = new MongoClient(mongoUri, { serverSelectionTimeoutMS: 5000 });
 
 export default client;
