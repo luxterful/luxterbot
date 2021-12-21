babc459c24a2197ccd486d25af45ccdcb71450f3
Lukas Bauer <lukas.bauer@vogel.de>
2021-12-21 07:37:28 +0100

    fix frontend port on local to 8080 and change on prod to 8080

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  lintOnSave: false
+if (!process.env.VUE_APP_SERVICE_1_URL) {
+  console.error("VUE_APP_SERVICE_1_URL environment variable not set");
+  process.exit(1);
 }
+
+module.exports = {
+  lintOnSave: false,
+  devServer: { port: 8080 },
+};
