6a811c600c924af85430d472d27f99211c3d381d
Lukas Bauer <lukas.bauer@vogel.de>
2021-12-21 13:26:34 +0100

    adds Message type to service-2

--- packages/service-2/src/controllers/webhook.ts
+++ packages/service-2/src/controllers/webhook.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { Message } from "service-1";
 
 const webhookRPC = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let message: string = req.body.message;
-
-    if (!message) {
-      return res.status(422).json({ error: "message must be set" });
-    }
+    const body: Message = req.body;
+    let message: string = body.message;
 
     message = message.replace(/\{\{\s*firstname\s*\}\}/, "Lukas");
     message = message.replace(/\{\{\s*lastname\s*\}\}/, "Bauer");
